244e23b2dfaf97aaea5beadfd29cef7f2e70d277
d3sp3r4t3Student <max.sesselmann.m.s@gmail.com>
2025-10-22 16:06:53 +0200

    trying to fix reverse order in dz when using auto_sleve=1

--- src/objects/domain_obj.F90
+++ src/objects/domain_obj.F90
@@ -1279,7 +1279,7 @@ contains
                     c  = min_lay_thckn - (a + b)
                     do jk = 1, nlevp1
                         jkr       = real(nlevp1-jk)       ! reverse index as function approaches top height at 0, and 0 at nz
-                        vct_a(jk) = a*jkr**3 + b*jkr**2 + c*jkr ! jk=1 is model bottom, jk=nz+1 is model top half-level
+                        vct_a(jkr) = a*jkr**3 + b*jkr**2 + c*jkr ! jk=1 is model bottom, jk=nz+1 is model top half-level
                     end do
 
                 case (2)
